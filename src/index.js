87def1e42463bd0e33d8ad3a3828237e8fbed57f
Henry Oberholtzer <henryoberholtzer@gmail.com>
2023-11-14 14:12:42 -0800

    add styling
    Co-authored-by: Noah Kise <noah@kisefamily.com>

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import './css/styles.css';
 import getGif from './js/gif-search';
 export { printError, displayResults };
 
-function printError(request, apiResponse, search) {
+const offsetAmount = 16;
+let offset = -offsetAmount;
+
+function printError(apiResponse, search) {
     document.querySelector('#error').innerText = `We ran into problems searching for ${search}: ${apiResponse.status} ${apiResponse.msg}`;
 }
 
@@ -13,7 +16,7 @@ function displayResults(apiResponse, search) {
     const resultPage = document.querySelector('#results');
     resultPage.innerHTML = "";
     if (Array.isArray(apiResponse.data)) {
-        apiResponse.data.forEach((data) => {
+        apiResponse.data.forEach((data, i) => {
             const div = document.createElement("div");
             const link = document.createElement("a");
             const img = document.createElement("img");
@@ -23,6 +26,11 @@ function displayResults(apiResponse, search) {
             link.append(img);
             div.append(link);
             resultPage.append(div);
+            if ((i + 1) % 4 === 0) {
+                const linebreak = document.createElement("div");
+                linebreak.setAttribute("class", "break");
+                resultPage.append(linebreak);
+            }
         });
     } else {
         const div = document.createElement("div");
@@ -39,9 +47,11 @@ function displayResults(apiResponse, search) {
 
 function handleFormSubmission(e) {
     e.preventDefault();
+    offset += offsetAmount;
     const search = document.querySelector("#search-string").value;
     const weird = document.getElementsByName("weird")[0].checked;
-    getGif(search, weird);
+    const evil = document.getElementsByName("evil")[0].checked;
+    getGif(search, [weird, evil], offset);
 }
 
 document.querySelector("form").addEventListener("submit", handleFormSubmission);
--- src/js/gif-search.js
+++ src/js/gif-search.js
@@ -1,24 +1,30 @@
 import { displayResults, printError } from "..";
 
-export default function getGif(search, weird) {
+export default function getGif(search, options, offset) {
     let request = new XMLHttpRequest();
-    const limit = 9;
-    const offset = 0;
-    const rating = "g";
+    const limit = 16;
+    // let offset = 0;
+    const rating = "r";
     const lang = "en";
-    const searchString = search;
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${searchString}&limit=${limit}&offset=${offset}&rating=${rating}&lang=${lang}&bundle=messaging_non_clips`;
-    const weirdUrl = `https://api.giphy.com/v1/gifs/translate?api_key=${process.env.API_KEY}&s=${searchString}&weirdness=10`;
+    const searchString = (search, options) => {
+        if (options[1] === true) {
+            return search + " EVIL";
+        } else {
+            return search;
+        }
+    };
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${searchString(search, options)}&limit=${limit}&offset=${offset}&rating=${rating}&lang=${lang}&bundle=messaging_non_clips`;
+    const weirdUrl = `https://api.giphy.com/v1/gifs/translate?api_key=${process.env.API_KEY}&s=${searchString(search, options)}&weirdness=5`;
     request.addEventListener("loadend", function () {
         const response = JSON.parse(this.responseText);
         if (this.status === 200) {
-            displayResults(response, search);
+            displayResults(response, searchString(search, options));
         } else {
-            printError(this, response, search);
+            printError(this, response, searchString(search, options));
         }
     });
 
-    if (weird === true) {
+    if (options[0] === true) {
         request.open("GET", weirdUrl, true);
     }   else {
         request.open("GET", url, true);
